5c2b34e14f6e697d363b859376dbfe7a24b123f1
Pan <dy2436@columbia.edu>
2022-11-10 16:27:04 -0800

    updated the writetablerule for basic forward

--- controller.py
+++ controller.py
@@ -35,44 +35,67 @@ def readTableRules(p4info_helper, sw):
             print('-----')
 
 
-def writeTunnelRules(p4info_helper, ingress_sw, egress_sw, tunnel_id,
-                     dst_eth_addr, dst_ip_addr):
+def writeTunnelRules(p4info_helper, ingress_sw, dst_eth_addr, dst_ip_addr, port):
+
+    # Write drop rule
     table_entry = p4info_helper.buildTableEntry(
         table_name="MyIngress.ipv4_lpm",
-        match_fields={
+        default_action=True,
+        action_name="MyIngress.drop",
+        action_params= {}
+    )
+    ingress_sw.WriteTableEntry(table_entry)
+
+    # Write ingress rule
+    table_entry = p4info_helper.buildTableEntry(
+        table_name="MyIngress.ipv4_lpm",
+        match_field={
             "hdr.ipv4.dstAddr": (dst_ip_addr, 32)
         },
-        action_name="MyIngress.myTunnel_ingress",
-        action_params={
-            "dst_id": tunnel_id,
-        })
-    ingress_sw.WriteTableEntry(table_entry)
-    print("Installed ingress tunnel rule on %s" % ingress_sw.name)
-
-    table_entry = p4info_helper.buildTableEntry(
-        table_name="MyIngress.myTunnel_exact",
-        match_fields={
-            "hdr.myTunnel.dst_id": tunnel_id
-        },
-        action_name="MyIngress.myTunnel_forward",
-        action_params={
-            "port": SWITCH_TO_SWITCH_PORT
-        })
-    ingress_sw.WriteTableEntry(table_entry)
-    print("Installed transit tunnel rule on %s" % ingress_sw.name)
-
-    table_entry = p4info_helper.buildTableEntry(
-        table_name="MyIngress.myTunnel_exact",
-        match_fields={
-            "hdr.myTunnel.dst_id": tunnel_id
-        },
-        action_name="MyIngress.myTunnel_egress",
+        action_name="MyIngress.ipv4_forward",
         action_params={
             "dstAddr": dst_eth_addr,
-            "port": SWITCH_TO_HOST_PORT
-        })
-    egress_sw.WriteTableEntry(table_entry)
-    print("Installed egress tunnel rule on %s" % egress_sw.name)
+            "port": port
+        }
+    )
+    ingress_sw.WriteTableEntry(table_entry)
+    
+    # table_entry = p4info_helper.buildTableEntry(
+    #     table_name="MyIngress.ipv4_lpm",
+    #     match_fields={
+    #         "hdr.ipv4.dstAddr": (dst_ip_addr, 32)
+    #     },
+    #     action_name="MyIngress.myTunnel_ingress",
+    #     action_params={
+    #         "dst_id": tunnel_id,
+    #     })
+    # ingress_sw.WriteTableEntry(table_entry)
+    # print("Installed ingress tunnel rule on %s" % ingress_sw.name)
+
+    # table_entry = p4info_helper.buildTableEntry(
+    #     table_name="MyIngress.myTunnel_exact",
+    #     match_fields={
+    #         "hdr.myTunnel.dst_id": tunnel_id
+    #     },
+    #     action_name="MyIngress.myTunnel_forward",
+    #     action_params={
+    #         "port": SWITCH_TO_SWITCH_PORT
+    #     })
+    # ingress_sw.WriteTableEntry(table_entry)
+    # print("Installed transit tunnel rule on %s" % ingress_sw.name)
+
+    # table_entry = p4info_helper.buildTableEntry(
+    #     table_name="MyIngress.myTunnel_exact",
+    #     match_fields={
+    #         "hdr.myTunnel.dst_id": tunnel_id
+    #     },
+    #     action_name="MyIngress.myTunnel_egress",
+    #     action_params={
+    #         "dstAddr": dst_eth_addr,
+    #         "port": SWITCH_TO_HOST_PORT
+    #     })
+    # egress_sw.WriteTableEntry(table_entry)
+    # print("Installed egress tunnel rule on %s" % egress_sw.name)
 
 def main(p4info_file_path, bmv2_file_path):
     # Instantiate a P4Runtime helper from the p4info file
@@ -109,8 +132,11 @@ def main(p4info_file_path, bmv2_file_path):
     s3.SetForwardingPipelineConfig(p4info=p4info_helper.p4info, bmv2_json_file_path=bmv2_file_path)
     s4.SetForwardingPipelineConfig(p4info=p4info_helper.p4info, bmv2_json_file_path=bmv2_file_path)
 
-    writeTunnelRules(p4info_helper, ingress_sw=s1, egress_sw=s2, tunnel_id=100, dst_eth_addr="08:00:00:00:02:22", dst_ip_addr="10.0.2.2/24")
-    writeTunnelRules(p4info_helper, ingress_sw=s2, egress_sw=s1, tunnel_id=200, dst_eth_addr="08:00:00:00:01:11", dst_ip_addr="10.0.1.1/24")
+    # setup the connection from s1 to s2
+    writeTunnelRules(p4info_helper, ingress_sw=s1, dst_eth_addr="08:00:00:00:02:22", dst_ip_addr="10.0.2.2", port=32)
+    # setup the connection from s2 to s1
+    writeTunnelRules(p4info_helper, ingress_sw=s2, dst_eth_addr="08:00:00:00:01:11", dst_ip_addr="10.0.1.1", port=32)
+    # writeTunnelRules(p4info_helper, ingress_sw=s2, egress_sw=s1, tunnel_id=200, dst_eth_addr="08:00:00:00:01:11", dst_ip_addr="10.0.1.1/24")
 
     # ShutdownAllSwitchConnections()
     print("close all switches connection, mininet 'h1 ping h2' stucks.")
