b5f1981c39fe8c20e36470d33d22d8aa06605b01
Pan <dy2436@columbia.edu>
2022-11-28 10:35:21 -0800

    fix stash conflict

--- controller.py
+++ controller.py
@@ -87,22 +87,6 @@ def deleteTableEntry(p4info_helper, ingress_sw, dst_eth_addr, dst_ip_addr, port,
     )
     ingress_sw.WriteTableEntry(table_entry)
 
-<<<<<<< HEAD
-def deleteTableEntry(p4info_helper, ingress_sw, dst_eth_addr, dst_ip_addr, port):
-    table_entry = p4info_helper.buildTableEntry(
-        table_name="MyIngress.ipv4_lpm",
-        match_fields={
-            "hdr.ipv4.dstAddr": (dst_ip_addr, 32)
-        },
-        action_name="MyIngress.ipv4_forward",
-        action_params={
-            "dstAddr": dst_eth_addr,
-            "port": port
-        }
-    )
-    ingress_sw.DeleteTableEntry(table_entry, False)
-=======
->>>>>>> stash-branch
 
 def main(p4info_file_path, bmv2_file_path):
     # Instantiate a P4Runtime helper from the p4info file
@@ -126,29 +110,6 @@ def main(p4info_file_path, bmv2_file_path):
 
     s1.MasterArbitrationUpdate()
     s2.MasterArbitrationUpdate()
-<<<<<<< HEAD
-    # s3.MasterArbitrationUpdate()
-    # s4.MasterArbitrationUpdate()
-
-    s1.SetForwardingPipelineConfig(p4info=p4info_helper.p4info, bmv2_json_file_path=bmv2_file_path)
-    s2.SetForwardingPipelineConfig(p4info=p4info_helper.p4info, bmv2_json_file_path=bmv2_file_path)
-
-
-
-    # # setup the connection from s1 to s2
-    writeTunnelRules(p4info_helper, ingress_sw=s1, dst_eth_addr="08:00:00:00:01:11", dst_ip_addr="10.0.1.1", port=1)
-    writeTunnelRules(p4info_helper, ingress_sw=s1, dst_eth_addr="08:00:00:00:02:22", dst_ip_addr="10.0.2.2", port=2)
-    # # setup the connection from s2 to s1
-    writeTunnelRules(p4info_helper, ingress_sw=s2, dst_eth_addr="08:00:00:00:01:11", dst_ip_addr="10.0.1.1", port=2)
-    writeTunnelRules(p4info_helper, ingress_sw=s2, dst_eth_addr="08:00:00:00:02:22", dst_ip_addr="10.0.2.2", port=1)
-
-    deleteTableEntry(p4info_helper, ingress_sw=s1, dst_eth_addr="08:00:00:00:02:22", dst_ip_addr="10.0.2.2", port=2)
-    deleteTableEntry(p4info_helper, ingress_sw=s2, dst_eth_addr="08:00:00:00:01:11", dst_ip_addr="10.0.1.1", port=2)
-
-
-    # readTableRules(p4info_helper, s1)
-    # readTableRules(p4info_helper, s2)
-=======
     s3.MasterArbitrationUpdate()
 
     s1.SetForwardingPipelineConfig(p4info=p4info_helper.p4info, bmv2_json_file_path=bmv2_file_path)
@@ -164,7 +125,6 @@ def main(p4info_file_path, bmv2_file_path):
     # setup the connection from s2 to s1
     writeTunnelRules(p4info_helper, ingress_sw=s2, dst_eth_addr="08:00:00:00:03:33", dst_ip_addr="10.0.3.3", port=3, dst_id=400)
     # writeTunnelRules(p4info_helper, ingress_sw=s2, egress_sw=s1, tunnel_id=200, dst_eth_addr="08:00:00:00:01:11", dst_ip_addr="10.0.1.1/24")
->>>>>>> stash-branch
 
     # ShutdownAllSwitchConnections()
     readTableRules(p4info_helper, s1)
